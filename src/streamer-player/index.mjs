7431aba2b2bd3d593117d7716289fb6214e48d90
Dennis Eriksson <denniseriksson93@live.com>
2025-08-14 14:20:11 +0200

    Start on streamer remote

--- src/streamer-player/index.mjs
+++ src/streamer-player/index.mjs
@@ -7,4 +7,4 @@ const browser = await puppeteer.launch({
 
 const page = await browser.newPage();
 
-await page.goto("http://localhost:3000");
+await page.goto("http://localhost:3000/streamer-client");
