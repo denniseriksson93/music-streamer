ee0f1a834e990e6c80679ede35ec800779ca0204
Dennis Eriksson <denniseriksson93@live.com>
2025-12-21 13:18:16 +0100

    divider is purly html and css

--- src/streamer-server/public/streamer-remote/index.js
+++ src/streamer-server/public/streamer-remote/index.js
@@ -1,7 +1,6 @@
 import { addDevice } from "./components/add-device/add-device.js";
 import { deviceSettings } from "./components/device-settings/device-settings.js";
 import { devices } from "./components/devices/devices.js";
-import { divider } from "./components/divider/divider.js";
 import { globalVolume } from "./components/global-volume/global-volume.js";
 import { header } from "./components/header/header.js";
 import { signInToSpotify } from "./services/sign-in-to-spotify.js";
@@ -12,9 +11,7 @@ const isSignedIn = await signInToSpotify();
 if (isSignedIn) {
   header.render();
   globalVolume.render();
-  divider.render("1");
   devices.render();
-  divider.render("2");
   addDevice.render();
 
   STATE.registerOnStateChanged("devices", globalVolume.render);
