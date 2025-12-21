f0a0481d0daeb75f9835252c9a8cd464518ee1fa
Dennis Eriksson <denniseriksson93@live.com>
2025-12-21 12:54:08 +0100

    enabled close button

--- src/streamer-server/public/streamer-remote/components/device-settings/device-settings.js
+++ src/streamer-server/public/streamer-remote/components/device-settings/device-settings.js
@@ -1,3 +1,5 @@
+import { iconElement } from "../../elements/icon-element.js";
+import { iconTextElement } from "../../elements/icon-text-element.js";
 import { STATE } from "../../services/state.js";
 
 export const deviceSettings = {
@@ -34,10 +36,35 @@ export const deviceSettings = {
     customNameContainer.appendChild(customNameLabel);
     customNameContainer.appendChild(customNameInput);
 
+    const settingsContainer = document.createElement("div");
+    settingsContainer.setAttribute("class", "settings-container");
+    settingsContainer.appendChild(name);
+    settingsContainer.appendChild(customNameContainer);
+
+    const closeButton = document.createElement("button");
+    closeButton.setAttribute("class", "full-width button-secondary");
+    closeButton.appendChild(iconTextElement("close", "Close"));
+    closeButton.addEventListener("click", () =>
+      STATE.set({ selectedDeviceId: undefined })
+    );
+
+    const saveAndCloseButton = document.createElement("button");
+    saveAndCloseButton.setAttribute("class", "full-width");
+    saveAndCloseButton.appendChild(iconTextElement("save", "Save"));
+
+    const buttonsContainer = document.createElement("div");
+    buttonsContainer.setAttribute("class", "buttons-container");
+    buttonsContainer.appendChild(closeButton);
+    buttonsContainer.appendChild(saveAndCloseButton);
+
+    const settingsButtonsContainer = document.createElement("div");
+    settingsButtonsContainer.setAttribute("class", "settings-button-container");
+    settingsButtonsContainer.appendChild(settingsContainer);
+    settingsButtonsContainer.appendChild(buttonsContainer);
+
     const contentContainer = document.createElement("div");
     contentContainer.setAttribute("class", "content-container");
-    contentContainer.appendChild(name);
-    contentContainer.appendChild(customNameContainer);
+    contentContainer.appendChild(settingsButtonsContainer);
 
     const mainContainer = document.createElement("div");
     mainContainer.setAttribute("class", "main-container");
