883829447a54e9c5c708bc204edd77c0bfeae2c0
Dennis Eriksson <denniseriksson93@live.com>
2025-12-28 23:15:19 +0100

    add devices when connected

--- src/streamer-server/services/devices-service.js
+++ src/streamer-server/services/devices-service.js
@@ -4,10 +4,12 @@ import { databaseRepository } from "./database-repository.js";
 
 const execAsync = promisify(exec);
 
+let numberOfConnectedDevices = 0;
+
 const getDevices = async () => {
   const { stdout } = await execAsync("pactl -f json list sinks");
 
-  /** @type {{ index: number, volume: { ['front-left']: { value_percent: string } } | { mono: { value_percent: string } }, properties: { ['api.bluez5.address']: string , ['device.alias']: string, ['device.bus']: string | undefined } }[]} */
+  /** @type {{ index: number, name: string, volume: { ['front-left']: { value_percent: string } } | { mono: { value_percent: string } }, properties: { ['api.bluez5.address']: string , ['device.alias']: string, ['device.bus']: string | undefined } }[]} */
   const connectedDevices = JSON.parse(stdout);
 
   const connectedBluetoothDevices = connectedDevices.filter(
@@ -36,6 +38,22 @@ const getDevices = async () => {
   }
 
   await databaseRepository.setData({ devices });
+
+  if (numberOfConnectedDevices !== connectedBluetoothDevices.length) {
+    await execAsync("pactl unload-module module-combine-sink");
+
+    const connectedBluetoothDevicesNames = connectedBluetoothDevices
+      .map(({ name }) => name)
+      .join();
+
+    await execAsync(
+      `pactl load-module module-combine-sink slaves=${connectedBluetoothDevicesNames} adjust_time=20 resample_method=trivial`
+    );
+
+    await execAsync("pactl set-default-sink combined");
+
+    numberOfConnectedDevices = connectedBluetoothDevices.length;
+  }
 
   return devices.map(({ bluetoothAddress, name, customName }) => {
     const connectedBluetoothDevice = connectedBluetoothDevices.find(
